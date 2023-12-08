c9dd1a74e2b6a3426a18878392775d6847c34dcc
Nicolas de Garrigues <ndegarrigues@gmail.com>
2023-12-08 16:54:06 +0100

    :wrench: UPDATE jsdoc NoteEnum

--- src/Note.ts
+++ src/Note.ts
@@ -2,7 +2,6 @@ class Note {
   private pitch: number;
 
   /**
-   * @param {NoteEnum} note
    * @param {number} octave - 0 to 8
    * @param {number} duration How long the note will be played in seconds. (you most likely want to stay under 1 second)
    */
@@ -25,11 +24,11 @@ class Note {
   ) {
     this.pitch = this.getPitch();
   }
-    
-	/**
-	 * @returns {number} The pitch of the note in Hz.
-	 */
-	public getPitch(): number {
+
+  /**
+   * @returns {number} The pitch of the note in Hz.
+   */
+  public getPitch(): number {
     enum NoteEnum {
       C = 0,
       CSharp = 1,
@@ -44,38 +43,47 @@ class Note {
       ASharp = 10,
       B = 11,
     }
-    
+
     let step = NoteEnum[this.note];
-		let power = Math.pow(2, (this.octave * 12 + step - 57)/12);
-		return 440 * power;
-	}
+    let power = Math.pow(2, (this.octave * 12 + step - 57) / 12);
+    return 440 * power;
+  }
 
-	public play(audioContext: AudioContext, oscillator: OscillatorType): void {
-		const oscillatorNode = this.getOscillator(audioContext, oscillator)
-		oscillatorNode.start(audioContext.currentTime)
-		setTimeout(() => {
-			oscillatorNode.stop(0);
-			oscillatorNode.disconnect();
-		}, this.duration * 1000);
-	}
+  public play(audioContext: AudioContext, oscillator: OscillatorType): void {
+    const oscillatorNode = this.getOscillator(audioContext, oscillator);
+    oscillatorNode.start(audioContext.currentTime);
+    setTimeout(() => {
+      oscillatorNode.stop(0);
+      oscillatorNode.disconnect();
+    }, this.duration * 1000);
+  }
 
-	public getOscillator(audioContext: AudioContext, oscillator: OscillatorType): OscillatorNode {
-		let gainNode: GainNode
-		let oscillatorNode: OscillatorNode
+  public getOscillator(
+    audioContext: AudioContext,
+    oscillator: OscillatorType,
+  ): OscillatorNode {
+    let gainNode: GainNode;
+    let oscillatorNode: OscillatorNode;
 
-		gainNode = audioContext.createGain()
-		gainNode.connect(audioContext.destination)
-		gainNode.gain.setValueAtTime(0, audioContext.currentTime)
-		gainNode.gain.linearRampToValueAtTime(0.15, audioContext.currentTime + 0.01)
-		gainNode.gain.linearRampToValueAtTime(0, audioContext.currentTime + this.duration - 0.01)
+    gainNode = audioContext.createGain();
+    gainNode.connect(audioContext.destination);
+    gainNode.gain.setValueAtTime(0, audioContext.currentTime);
+    gainNode.gain.linearRampToValueAtTime(
+      0.15,
+      audioContext.currentTime + 0.01,
+    );
+    gainNode.gain.linearRampToValueAtTime(
+      0,
+      audioContext.currentTime + this.duration - 0.01,
+    );
 
-		oscillatorNode = audioContext.createOscillator()
-		oscillatorNode.connect(gainNode)
-		oscillatorNode.type = oscillator
-		oscillatorNode.frequency.value = this.getPitch()
+    oscillatorNode = audioContext.createOscillator();
+    oscillatorNode.connect(gainNode);
+    oscillatorNode.type = oscillator;
+    oscillatorNode.frequency.value = this.getPitch();
 
-		return oscillatorNode
-	}
+    return oscillatorNode;
+  }
 }
 
 export { Note };
