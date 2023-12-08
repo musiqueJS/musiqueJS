99337c6f88cd5398bbc60c0fdf982cc28ae39083
Julien Cousin-Alliot <julien.cousinalliot@gmail.com>
2023-12-08 16:44:51 +0100

    feat(note): Add play method to notes & chords (#17)

--- src/CustomChord.ts
+++ src/CustomChord.ts
@@ -7,6 +7,21 @@ class CustomChord {
     constructor(
         public notes: Note[],
     ) {}
+
+    public play(audioContext: AudioContext, oscillator: OscillatorType): void {
+        for (const note of this.notes) {
+            note.play(audioContext, oscillator)
+        }
+    }
+
+    public getOscillatorNodes(audioContext: AudioContext, oscillator: OscillatorType): { oscillatorNode: OscillatorNode, noteDuration: number }[] {
+        return this.notes.map((note) => {
+            const noteDuration: number = note.duration
+            const oscillatorNode: OscillatorNode = note.getOscillator(audioContext, oscillator)
+
+            return { oscillatorNode, noteDuration }
+        })
+    }
 }
 
 export { CustomChord }
