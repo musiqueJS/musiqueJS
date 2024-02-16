a85142aa3df685686b00465e9a47432f0c59d557
Julien COUSIN-ALLIOT <julien.cousinalliot@gmail.com>
2024-02-16 14:11:27 +0100

    WIP: Implement chords

--- src/CustomChord.ts
+++ src/CustomChord.ts
@@ -1,26 +1,29 @@
 import {Note} from "./Note";
+import PlayableInterface from "./PlayableInterface";
 
-class CustomChord {
+class CustomChord implements PlayableInterface {
     /**
      * @param {Note[]} notes - The notes that will be played simultaneously.
+     * @param {number} duration - How long the chord will be played in seconds. (you most likely want to stay under 1 second)
      */
     constructor(
         public notes: Note[],
+        public duration?: number
     ) {}
 
-    public play(audioContext: AudioContext, oscillator: OscillatorType): void {
+    public play(audioContext: AudioContext, oscillator: OscillatorType, resolve: () => void = () => {}): void {
         for (const note of this.notes) {
-            note.play(audioContext, oscillator)
+            // if (this.duration) {
+            //     note.duration = this.duration;
+            // }
+
+            note.play(audioContext, oscillator, resolve)
         }
-    }
 
-    public getOscillatorNodes(audioContext: AudioContext, oscillator: OscillatorType): { oscillatorNode: OscillatorNode, noteDuration: number }[] {
-        return this.notes.map((note) => {
-            const noteDuration: number = note.duration
-            const oscillatorNode: OscillatorNode = note.getOscillator(audioContext, oscillator)
+        setTimeout(() => {
+            resolve();
 
-            return { oscillatorNode, noteDuration }
-        })
+        }, 3 * 1000);
     }
 }
 
