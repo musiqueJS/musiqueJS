abd8626e5486212ca24e2a91e48463bc5d306814
Julien COUSIN-ALLIOT <julien.cousinalliot@gmail.com>
2024-04-19 15:43:20 +0200

    Add JSONPartition

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
+// Class
 export { Note } from "./src/Note";
 export { Partition } from "./src/Partition";
 export { CustomChord } from "./src/CustomChord";
 export { JSONPartition } from "./src/JSONPartition";
+
+// Interface
+export { JSONNote, JSONMusic } from "./src/interface/JSONPlayable";
--- src/JSONPartition.ts
+++ src/JSONPartition.ts
@@ -1,32 +1,44 @@
 import { Partition } from "./Partition";
 import { Note } from "./Note";
-
-interface JSONNote {
-  note: string;
-  duration: number;
-  octave: number;
-}
+import { CustomChord } from "./CustomChord";
+import { JSONNote, JSONMusic } from "./interface/JSONPlayable";
 
 class JSONPartition extends Partition {
   /**
-   * @param {JSONNote[]} json - The JSON that will be parsed into a partition.
+   * @param {JSONMusic[]} json - The JSON that will be parsed into a partition.
    * @param {OscillatorType} oscillator - The oscillator type that will be used to play the partition.
    * @param {AudioContext} audioContext - The audio context that will be used to play the partition.
    */
   constructor(
-    public json: JSONNote[],
+    public json: JSONMusic[],
     public oscillator: OscillatorType = "sine",
     public audioContext: AudioContext,
   ) {
     let jsonNotes: Note[] = [];
 
-    for (const note of json) {
-      // @ts-ignore
-      jsonNotes.push(new Note(note.note, note.octave, note.duration));
+    for (const playable of json) {
+      if (isJSONNote(playable)) {
+        // @ts-ignore
+        jsonNotes.push(new Note(playable.note, playable.octave, playable.duration));
+      } else if (isJSONChord(playable)) {
+        console.log('chord', playable);
+        // @ts-ignore
+        jsonNotes.push(new CustomChord(playable.map((note) => new Note(note.note, note.octave, note.duration))));
+      } else {
+        throw new Error("Invalid JSONMusic");
+      }
     }
 
     super(jsonNotes, oscillator, audioContext);
   }
+}
+
+function isJSONNote(obj: any): obj is JSONNote {
+  return 'note' in obj && 'duration' in obj && 'octave' in obj;
+}
+
+function isJSONChord(obj: any): obj is JSONNote[] {
+  return Array.isArray(obj) && obj.every(isJSONNote);
 }
 
 export { JSONPartition };
