1fcfb499235e20ca358070b28afd0fdf5d87eee3
Braunschweig Malo <70762378+AluSinVIII@users.noreply.github.com>
2024-07-15 11:18:11 +0200

    Update CustomOscillatorType.ts

--- src/CustomOscillatorType.ts
+++ src/CustomOscillatorType.ts
@@ -1,3 +1,3 @@
-type CustomOscillatorType = 'sine' | 'square' | 'sawtooth' | 'triangle' | 'piano' ;
+type CustomOscillatorType = 'sine' | 'square' | 'sawtooth' | 'triangle' | 'piano' | 'betterpiano' | 'bestpiano' | 'guitar' ;
 
 export default CustomOscillatorType;
